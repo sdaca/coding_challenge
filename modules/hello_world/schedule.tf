6350e8b8f3c56b311879579055cb073c933b92a1
Jens Dieskau <jens@dieskau.pm>
2022-11-08 08:38:34 +0100

    feat(hello_world): configure cloudwatch trigger for `hello_world` lambda module
    Use the `trigger_rate` variable to let cloudwatch trigger the `hello_world` lambda
    module in a certain interval. Unit is minutes. Default is 60 minutes.

---
+++ modules/hello_world/schedule.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_event_rule" "lambda_cron" {
+  count = var.enabled ? 1 : 0
+
+  name                = "${var.prefix}_hello_world"
+  schedule_expression = "rate(${var.rate} minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "cron" {
+  count = var.enabled ? 1 : 0
+
+  rule      = aws_cloudwatch_event_rule.lambda_cron[0].name
+  target_id = "${var.prefix}_hello_world"
+  arn       = aws_lambda_function.function[0].arn
+}
+
+resource "aws_lambda_permission" "allow_execution_from_cloudwatch" {
+  count = var.enabled ? 1 : 0
+
+  statement_id  = "AllowExecutionFromCloudWatch_${var.prefix}_${aws_lambda_function.function[0].function_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.function[0].function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_cron[0].arn
+}
