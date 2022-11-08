aa3f478044eb131c9134bc67bdfeac959eb6b1f6
Jens Dieskau <jens@dieskau.pm>
2022-11-08 08:38:34 +0100

    feat: configure cloudwatch trigger for hello_world lambda module

--- modules/hello_world/variables.tf
+++ modules/hello_world/variables.tf
@@ -3,6 +3,11 @@ variable "prefix" {
   type        = string
 }
 
+variable "trigger_rate" {
+  description = "Rate [in minutes] in which the lambda function should be invoked"
+  type        = number
+  default     = 60
+}
 
 variable "enabled" {
   description = "Enable or disable lambda function"
