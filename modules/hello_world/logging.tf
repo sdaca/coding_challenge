18ba4488c825be9f4214b9419b8163a31b242b49
Jens Dieskau <jens@dieskau.pm>
2022-11-08 08:40:50 +0100

    feat: let terraform manage lambda logging group
    Normally, a lambda function would automatically create a new logging group in
    cloudwatch. This log group will therefore not be managed by terraform. It will
    not be deleted on removing the lambda function and only has default configuration
    parameter (i.e. rentention time would be infinite).
    
    The solution is to deny all lambda function to create a new log group
    (`logs:CreateLogGroup`) and create the group with terraform itself.

---
+++ modules/hello_world/logging.tf
@@ -0,0 +1,16 @@
+data "aws_iam_policy_document" "lambda_exec_role_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "loggroup" {
+  name              = "/aws/lambda/${aws_lambda_function.function.function_name}"
+  retention_in_days = 14
+}
